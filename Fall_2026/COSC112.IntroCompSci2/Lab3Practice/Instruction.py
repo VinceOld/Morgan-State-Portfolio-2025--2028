6edfb4fd5ecab52c130f841ea60cfbe6b0986c00
VinceYoung <106188228+VinceOld@users.noreply.github.com>
2026-09-20 22:00:40 -0400

    Create Instruction.py

---
+++ Fall_2026/COSC112.IntroCompSci2/Lab3Practice/Instruction.py
@@ -0,0 +1,62 @@
+String, conditionals, and loop practice
+String, Conditionals and Loop Practice (Due: Sep 21st, 11:59 PM)
+Task 1 (15pts) - Character Counter
+Write a Python program that prompts the user to enter a sentence.
+
+The program should count and display:
+
+The total number of characters (excluding spaces).
+
+The number of vowels (a, e, i, o, u).
+
+The number of consonants.
+
+Use a loop to iterate through the string and conditional statements to classify characters.
+
+Test your function with 3 cases
+
+Example input and output:
+
+
+Enter a sentence: Python is fun!
+Total characters (excluding spaces): 12  
+Vowel count: 3  
+Consonant count: 8  
+Enter a sentence: Data Science is amazing!
+Total characters (excluding spaces): 21  
+Vowel count: 9  
+Consonant count: 11  
+
+Task 2 (15pts) - Reversing Words in a Sentence
+Write a Python program that prompts the user to enter a sentence.
+
+The program should reverse the order of the words while keeping the characters in each word unchanged.
+
+Use the .split() method to split the string into words and loop structures to reconstruct the sentence.
+
+Test your function with 3 cases
+
+Example input and output:
+
+Enter a sentence: Learning Python is fun
+Reversed sentence: fun is Python Learning
+
+
+Task 3 (20pts) - Palindrome Checker
+
+Write a Python program that asks the user to enter a word.
+
+The program should check whether the word is a palindrome (a word that reads the same forward and backward).
+
+Ignore capitalization (treat "Racecar" as a palindrome).
+
+Test your function with 3 cases
+
+Example input and output:
+
+Enter a word: Racecar
+It is a palindrome!
+Enter a word: helloworld
+It is not a palindrome!
+Enter a word: EustonsawIwasnotSue
+It is a palindrome!
